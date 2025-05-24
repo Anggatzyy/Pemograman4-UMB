ddf2cd441b1918886eede0e3099f838d7ef08fa0
Acer <dwianggararamadhan@gmail.com>
2025-05-25 00:29:41 +0700

    add all in here

---
+++ app/src/main/java/com/example/firstproject/homeactivity.java
@@ -0,0 +1,12 @@
+package com.example.firstproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class homeactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity); // Pastikan file ini ada di res/layout/
+    }
+}
